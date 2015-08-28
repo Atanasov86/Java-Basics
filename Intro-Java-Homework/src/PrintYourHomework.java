bfdd059dddde36ec19658d7aae01d4ef5b7ac7f2
xenonbg <anton.atanasov86@gmail.com>
2015-08-28 19:05:10 +0300

    Added 01.Intro-Java-Homework

---
+++ Intro-Java-Homework/src/PrintYourHomework.java
@@ -0,0 +1,6 @@
+public class PrintYourHomework {
+    public static void main(String[] args) {
+        String homeworkName = "Intro-Java-Homework";
+        System.out.println(homeworkName);
+    }
+}
