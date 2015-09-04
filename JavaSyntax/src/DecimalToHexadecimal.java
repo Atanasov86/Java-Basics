8d2f5fc96f76e700358adcc249a43ebc3708d442
xenonbg <anton.atanasov86@gmail.com>
2015-09-04 13:34:30 +0300

    added some changes and Problems requirement

--- JavaSyntax/src/DecimalToHexadecimal.java
+++ JavaSyntax/src/DecimalToHexadecimal.java
@@ -4,6 +4,6 @@ public class DecimalToHexadecimal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println(Integer.toHexString(number));
+        System.out.println(Integer.toHexString(number).toUpperCase());
     }
 }
